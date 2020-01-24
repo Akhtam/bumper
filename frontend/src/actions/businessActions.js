97e96a099d5dc19c674799e3b80205187b5783c2
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-23 17:51:24 -0800

    create business action

--- frontend/src/actions/businessActions.js
+++ frontend/src/actions/businessActions.js
@@ -1,15 +1,16 @@
 import * as businessApiUtil from '../util/businessUtil';
+import { receiveCurrentUser } from './sessionActions';
 
 export const RECEIVE_BUSINESS = 'RECEIVE_BUSINESS';
 
-const receiveBusiness = ({business, services}) => ({
+const receiveBusiness = ({ business, services }) => ({
 	type: RECEIVE_BUSINESS,
-    business,
-    services
+	business,
+	services
 });
 
 export const fetchBusiness = providerId => dispatch => {
 	return businessApiUtil
 		.fetchbusiness(providerId)
-		.then(res => console.log(res));
+		.then(res => dispatch(receiveCurrentUser(res.data)));
 };
