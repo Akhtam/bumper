41f62cec0db62cccc828d939f8e7665600ba527c
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-26 19:35:54 -0800

    .

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	mongoURI:
-		'mongodb+srv://dev:Rz19YGtNWbi1bYB5@cluster1-0ptus.mongodb.net/test?retryWrites=true&w=majority',
-	secretOrKey: 'Icz49y1Zic3PLaMxRY84cF3SGp9sUJje'
-};
