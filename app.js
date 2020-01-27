93581046d7069d18d6403b2cc5dc4b2041cce997
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-26 19:49:52 -0800

    add env

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const users = require("./routes/api/users");
-const businesses = require("./routes/api/businesses");
-const services = require("./routes/api/services");
-const appointments = require("./routes/api/appointments");
+const users = require('./routes/api/users');
+const businesses = require('./routes/api/businesses');
+const services = require('./routes/api/services');
+const appointments = require('./routes/api/appointments');
 const path = require('path');
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('Connected to MongoDB successfully'))
+	.catch(err => console.log(err));
+
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('frontend/build'));
 	app.get('/', (req, res) => {
@@ -21,18 +24,13 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
-mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
-
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use("/api/users", users);
-app.use("/api/businesses", businesses);
-app.use("/api/services", services);
-app.use("/api/appointments", appointments);
+app.use('/api/users', users);
+app.use('/api/businesses', businesses);
+app.use('/api/services', services);
+app.use('/api/appointments', appointments);
