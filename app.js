6c3c48b0a11e77640ba99a1dcc641935a0143053
Akhtam Ismatov <aismatov1991@gmail.com>
2020-01-27 18:01:01 -0800

    fix bug for localhost

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ if (process.env.NODE_ENV === 'production') {
 		);
 	});
 }
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`listening on port ${port}!`));
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
