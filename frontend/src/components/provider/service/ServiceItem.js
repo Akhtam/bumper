22a85e24d87b0afa3f98df4d753e948ecc2ba0bf
Akhtam Ismatov <aismatov1991@gmail.com>
2020-02-03 15:20:55 -0800

    fix service bug

--- frontend/src/components/provider/service/ServiceItem.js
+++ frontend/src/components/provider/service/ServiceItem.js
@@ -4,23 +4,27 @@ import "./service.scss";
 
 export const ServiceItem = props => {
 	
-  if (!props.service || props.service._id === '5e3872eb7ca596efa3352645') {
-		return null;
-  } else {
+
 		return (
 			<div className='ServiceItem'>
 				<img src='https://moovingdev.fr/wp-content/uploads/2018/01/Control-Panel.png' />
 				<div className='servicePrice'>{props.service.price}</div>
 				<div className='serviceType'>
-					{props.service.type}
-					<button
-						className='ServiceButtons'
-						onClick={() => props.deleteService(props.service._id)}
-					>
-						Delete
-					</button>
+					{props.service.type}{' '}
+					{props.service._id === '5e3872eb7ca596efa3352645' ? (
+						''
+					) : (
+						<button
+							className='ServiceButtons'
+							onClick={() =>
+								props.deleteService(props.service._id)
+							}
+						>
+							Delete
+						</button>
+					)}
 				</div>
 			</div>
 		);
-  }
+  
 };
